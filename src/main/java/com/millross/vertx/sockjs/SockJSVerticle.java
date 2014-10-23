8ae0376a41a1265c7760e8133724c712caf79f4e
jez <mr.nonpc@gmail.com>
2014-10-23 19:57:25 +0100

    Initial version with trivial sockjs server, now about to add a test

---
+++ src/main/java/com/millross/vertx/sockjs/SockJSVerticle.java
@@ -0,0 +1,31 @@
+package com.millross.vertx.sockjs;
+
+import org.vertx.java.core.buffer.Buffer;
+import org.vertx.java.core.http.HttpServer;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.core.sockjs.SockJSServer;
+import org.vertx.java.platform.Verticle;
+
+/**
+ * User: jez
+ */
+public class SockJSVerticle extends Verticle {
+
+    @Override
+    public void start() {
+        super.start();
+        HttpServer httpServer = vertx.createHttpServer();
+        SockJSServer server = vertx.createSockJSServer(httpServer);
+
+        server.installApp(new JsonObject().putString("prefix", "/testsocket"), socket -> {
+            socket.write(new Buffer("HELLO"));
+
+            socket.dataHandler(buf -> {
+                System.out.println(buf.toString());
+                socket.write(new Buffer("Thankyou"));
+            });
+        });
+
+        httpServer.listen(8080, "0.0.0.0");
+    }
+}
